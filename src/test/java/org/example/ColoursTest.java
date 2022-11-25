9e4381b93588301be3c519e54fce344966b592ac
OliverLinger <olienew32@gmail.com>
2022-11-25 09:59:58 +0000

    Within coloursTest.java I have created a test to test whether the variables red, green and blue are the correct floating point numbers. It fails at this stage as it has no getters for red, green and blue.

--- src/test/java/org/example/ColoursTest.java
+++ src/test/java/org/example/ColoursTest.java
@@ -2,16 +2,33 @@ package org.example;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColoursTest {
+    Colours colours;
 
     @BeforeEach
     void setUp() {
+        colours = new Colours();
     }
 
     @AfterEach
     void tearDown() {
+    }
+
+    /*
+    This test function tests whether the floating point representations of red, green and blue are the correct values.
+    red should be 0.0,
+    green should be 0.1,
+    blue should be 1.0.
+    Using assertEquals we can test this.
+     */
+    @Test
+    void createColourTest() {
+        assertArrayEquals(0.0, colours.getRed());
+        assertArrayEquals(0.1, colours.getGreen());
+        assertArrayEquals(1.0, colours.getBlue());
     }
 }

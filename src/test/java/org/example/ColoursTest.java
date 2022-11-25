68814d5498584af16cce40613e52f4b6c981b5c5
OliverLinger <olienew32@gmail.com>
2022-11-25 16:40:38 +0000

    I rewrote the test method createColourTest() it now uses assertTrue to find out if the boolean values are within the range of 0.0 and 1.0.

--- src/test/java/org/example/ColoursTest.java
+++ src/test/java/org/example/ColoursTest.java
@@ -7,8 +7,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColoursTest {
+    /*
+    Declaring an instance of the Colours class called colours.
+     */
     Colours colours;
 
+    /*
+    A new instance of Colours() is created before and testing occurs.
+     */
     @BeforeEach
     void setUp() {
         colours = new Colours();
@@ -20,15 +26,21 @@ class ColoursTest {
 
     /*
     This test function tests whether the floating point representations of red, green and blue are the correct values.
-    red should be 0.0,
-    green should be 0.1,
-    blue should be 1.0.
-    Using assertEquals we can test this.
+    Red, Green, and Blue should be within the range of 0.0 and 1.0.
      */
     @Test
     void createColourTest() {
-        assertArrayEquals(0.0, colours.getRed());
-        assertArrayEquals(0.1, colours.getGreen());
-        assertArrayEquals(1.0, colours.getBlue());
+        // assert that the red value is less than 1.0.
+        assertTrue(colours.getRed() < 1.0 ,"Value for red is too large and not within the range");
+        // assert that the red value is greater than 0.0
+        assertTrue(colours.getRed() < 0.0 ,"Value for red is too small and not within the range");
+        // assert that the green value is less than 1.0
+        assertTrue(colours.getGreen() < 1.0 ,"Value for green is too large and not within the range");
+        // assert that the green value is greater than 0
+        assertTrue(colours.getGreen() < 0.0 ,"Value for green is too small and not within the range");
+        // assert that the blue value is less than 1.0.
+        assertTrue(colours.getBlue() < 1.0 ,"Value for blue is too large and not within the range");
+        // assert that the blue value is greater than 0.0
+        assertTrue(colours.getBlue() < 0.0 ,"Value for blue is too small and not within the range");
     }
 }

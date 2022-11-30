0d76986b80da4819555db78892b3feacde68bcd1
OliverLinger <olienew32@gmail.com>
2022-11-30 10:13:42 +0000

    I refactored the file system so that there was no test file for Main as it wasn't necessary. Main is not tested since it is currently only calling the single RGBColour method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Colour colour;
-        colour = new Colour(0.1f, 0.1f, 0.7f);
+        colour = new Colour(1.0f, 1.0f, 1.0f);
+        System.out.println(colour.RGBColour());
     }
 }
